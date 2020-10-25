5fb49d01e59f37f207170ba310f50a78baadae90
Timothy Jones <t.jones@aptacode.com>
2020-10-25 19:26:01 +0000

    Added EqualToTestSkeleton

---
+++ Expressions.Tests/Boolean/Comparison/EqualToTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Expressions.Tests.Boolean.Comparison
+{
+    public class EqualToTests
+    {
+        [Fact]
+        public void EqualToTest()
+        {
+            //Arrange 
+
+            //Act 
+
+            //Assert
+        }
+    }
+}
