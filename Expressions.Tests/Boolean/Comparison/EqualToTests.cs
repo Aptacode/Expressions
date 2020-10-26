757fde27aed09cfebae7d96c9f2cf21f9ba6980c
Timothy Jones <t.jones@aptacode.com>
2020-10-26 00:32:49 +0000

    Updated csproj

--- Expressions.Tests/Boolean/Comparison/EqualToTests.cs
+++ Expressions.Tests/Boolean/Comparison/EqualToTests.cs
@@ -1,9 +1,19 @@
-﻿using Xunit;
+﻿using Aptacode.Expressions;
+using Aptacode.Expressions.Bool;
+using Aptacode.Expressions.Bool.Comparison;
+using Aptacode.Expressions.Integer;
+using Xunit;
 
 namespace Expressions.Tests.Boolean.Comparison
 {
+    public class TestContext : IContext
+    {
+
+    }
     public class EqualToTests
     {
+
+        
         [Fact]
         public void EqualToTest()
         {
