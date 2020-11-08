32a631f61c715843b8b5443573393b79ee1c40ce
Timothy Jones <t.jones@aptacode.com>
2020-11-08 12:14:35 +0000

    Added ConstantColor expression

---
+++ Expressions/Color/ConstantColor.cs
@@ -0,0 +1,16 @@
+﻿using Aptacode.Expressions.String;
+
+namespace Aptacode.Expressions.Color
+{
+    public class ConstantColor<TContext> : IColorExpression<TContext> where TContext : IContext
+    {
+        public ConstantColor(System.Drawing.Color value)
+        {
+            Value = value;
+        }
+
+        public System.Drawing.Color Value { get; }
+
+        public System.Drawing.Color Interpret(TContext context) => Value;
+    }
+}
