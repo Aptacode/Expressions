1bffc68701fc77ee9c82b48aa0743713d0658b93
Timothy Jones <t.jones@aptacode.com>
2020-11-17 19:51:21 +0000

    Added ConditionalInteger expression

---
+++ Expressions/Integer/ConditionalInteger.cs
@@ -0,0 +1,10 @@
+﻿using Aptacode.Expressions.Bool;
+using Aptacode.Expressions.Numeric;
+
+namespace Aptacode.Expressions.Integer
+{
+    public class ConditionalInteger<TContext> : ConditionalNumeric<int, TContext>, IIntegerExpression<TContext>
+    {
+        public ConditionalInteger(IBooleanExpression<TContext> condition, IIntegerExpression<TContext> lhs, IIntegerExpression<TContext> rhs) : base(condition, lhs, rhs) { }
+    }
+}
